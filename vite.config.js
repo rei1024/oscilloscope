50cd32051802f38203a78eb23858d50c9fa63753
Rei <satopen1729@gmail.com>
2025-09-26 20:11:24 +0900

    fix: build for changelog page (#27)

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import { resolve } from "node:path";
+
 process.env.VITE_DATE = new Date().toISOString();
 
 /** @type {import('vite').UserConfig} */
@@ -5,5 +7,13 @@ export default {
   base: "/oscilloscope",
   server: {
     port: 5174,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        changelog: resolve(__dirname, "pages/changelog/index.html"),
+      },
+    },
   },
 };
