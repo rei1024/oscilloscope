85de51d00ed700cd1367d8f4f60fcae5e153ea87
rei1024 <satopen1729@gmail.com>
2025-09-27 10:53:34 +0900

    feat: Support more History and Super rules

--- src/lib/findPeriod.test.ts
+++ src/lib/findPeriod.test.ts
@@ -1,19 +1,26 @@
 import { describe, it, expect } from "vitest";
-import { findPeriod } from "./findPeriod";
+import { findPeriod, findPeriodUint8 } from "./findPeriod";
 
 describe("findPeriod", () => {
   it("correct", () => {
-    expect(findPeriod([])).toEqual(0);
-    expect(findPeriod([1])).toEqual(1);
-    expect(findPeriod([1, 1])).toEqual(1);
-    expect(findPeriod([1, 0])).toEqual(2);
-    expect(findPeriod([1, 0, 1])).toEqual(3);
-    expect(findPeriod([1, 1, 1])).toEqual(1);
-    expect(findPeriod([0, 0, 0])).toEqual(1);
-    expect(findPeriod([1, 0, 1, 0])).toEqual(2);
-    expect(findPeriod([1, 0, 1, 0, 1, 0])).toEqual(2);
-    expect(findPeriod([1, 0, 0, 1, 1, 0])).toEqual(6);
-    expect(findPeriod([1, 0, 0, 1, 1, 0, 0, 1])).toEqual(4);
+    const items = [
+      { input: [], expected: 0 },
+      { input: [1], expected: 1 },
+      { input: [1, 1], expected: 1 },
+      { input: [1, 0], expected: 2 },
+      { input: [1, 0, 1], expected: 3 },
+      { input: [1, 1, 1], expected: 1 },
+      { input: [0, 0, 0], expected: 1 },
+      { input: [1, 0, 1, 0], expected: 2 },
+      { input: [1, 0, 1, 0, 1, 0], expected: 2 },
+      { input: [1, 0, 0, 1, 1, 0], expected: 6 },
+      { input: [1, 0, 0, 1, 1, 0, 0, 1], expected: 4 },
+    ];
+
+    for (const { input, expected } of items) {
+      expect(findPeriod(input)).toEqual(expected);
+      expect(findPeriodUint8(new Uint8Array(input))).toEqual(expected);
+    }
   });
 
   it("correct string", () => {
